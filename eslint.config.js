4471ab121514287045e484d1c4aae76f9f863c83
Marius Szabo <szabomarius@gmail.com>
2025-06-20 10:25:40 +0300

    fix: stop formatting lock json

--- eslint.config.js
+++ eslint.config.js
@@ -7,30 +7,28 @@ import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default [
-    ...tseslint.config(
-        { ignores: ['dist'] },
-        {
-            extends: [js.configs.recommended, ...tseslint.configs.recommended],
-            files: ['**/*.{ts,tsx}'],
-            languageOptions: {
-                ecmaVersion: 2020,
-                globals: globals.browser,
-            },
-            plugins: {
-                'react-hooks': reactHooks,
-                'react-refresh': reactRefresh,
-                'simple-import-sort': simpleImportSort,
-            },
-            rules: {
-                ...reactHooks.configs.recommended.rules,
-                'react-refresh/only-export-components': [
-                    'warn',
-                    { allowConstantExport: true },
-                ],
-                'simple-import-sort/imports': 'error',
-                'simple-import-sort/exports': 'error',
-            },
-        }
-    ),
+    { ignores: ['dist', 'package-lock.json'] },
+    ...tseslint.config({
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'simple-import-sort': simpleImportSort,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
+    }),
     eslintConfigPrettier,
 ];
