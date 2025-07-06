6d2f3fb11440ccfca0716e6887388d00e1599f83
Marius Szabo <szabomarius@gmail.com>
2025-07-06 10:02:55 +0300

    fix: lint

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom/vitest';
+
+// Mock Worker for tests
+class MockWorker {
+    constructor() {}
+    postMessage() {}
+    terminate() {}
+    addEventListener() {}
+    removeEventListener() {}
+    onmessage = null;
+    onerror = null;
+}
+
+// @ts-expect-error - Mocking Worker for tests
+global.Worker = MockWorker;
